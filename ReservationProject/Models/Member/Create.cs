a1ff38632293985a9f40557f3ac37dd2702b3db6
LeeHikari <liam.isle@gmail.com>
2021-10-16 13:04:04 +1100

    Phone prop added to Person.cs

--- ReservationProject/Models/Member/Create.cs
+++ ReservationProject/Models/Member/Create.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,16 @@ namespace ReservationProject.Models.Member
 {
     public class Create
     {
-        
+        [Required(ErrorMessage = "Lastname: Required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Firstname: Required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Email: Required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone: Required")]
+        public string Phone { get; set; }
     }
 }
