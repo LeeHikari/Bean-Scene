f40e36888efff33eee5776775984b56907a3d57a
StevenDiploma <stevengweston@hotmail.com>
2021-10-17 15:10:34 +1100

    On-going

---
+++ ReservationProject/Areas/Member/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ReservationProject.Data;
+using ReservationProject.Areas.Member.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationProject.Areas.Member.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<IdentityUser> userManager, IMapper mapper)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+            _mapper = mapper;
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
+
+                var person = await _context.People.FirstOrDefaultAsync(p => p.UserId == user.Id);
+
+                if (!User.IsInRole("Employee"))
+                {
+                    await _userManager.AddToRoleAsync(user, "Employee");
+                }
+            }
+
+            var model = new Models.Reservation.Index
+            {
+                SittingTypes = new SelectList(_context.Sittings.ToArray(), nameof(Sitting.Id), nameof(Sitting.Name))
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Models.Reservation.Index model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var reservation = _mapper.Map<Data.Reservation>(model);
+                    _context.Reservations.Add(reservation);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    ex.Message.ToString();
+                }
+            }
+
+            model.SittingTypes = new SelectList(_context.Sittings.ToArray(), nameof(Sitting.Id), nameof(Sitting.Name));
+            return View(model);
+        }
+    }
+}
--- ReservationProject/Areas/Member/Models/Home/Index.cs
+++ ReservationProject/Areas/Member/Models/Home/Index.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ReservationProject.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -26,8 +28,15 @@ namespace ReservationProject.Areas.Member.Models.Reservation
         public int Guests { get; set; }
 
         public string Note { get; set; }
-        public int SittingId { get; set; }
+        public Sitting Sitting { get; set; }
         public int ReservationStatusId { get; set; }
         public int ReservationSourceId { get; set; }
+
+        [Display(Name = "Sitting Types")]
+        public int SittingId { get; set; }
+
+        public SelectList SittingTypes { get; set; }
+
+
     }
 }
