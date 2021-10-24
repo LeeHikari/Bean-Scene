eb76318664bb1995d7d315a0866e7df596b8c52d
DavidWHY312 <david.yau1@hotmail.com>
2021-10-24 13:48:59 +1100

    Sitting Allows to create but controller still needs to be fixed

--- ReservationProject/Areas/Admin/Controllers/SittingController.cs
+++ ReservationProject/Areas/Admin/Controllers/SittingController.cs
@@ -1,28 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ReservationProject.Data;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ReservationProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class SittingController : Controller
+    public class SittingController : AdminAreaBaseController
     {
-        public IActionResult Index()
+
+
+        public SittingController(ApplicationDbContext context)
+           : base(context)
         {
-            return View();
+
         }
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+        
+            public IActionResult Index()
+            {
+                return View();
+            }
 
-        public IActionResult Delete()
-        {
-            return View();
-        }
+            [HttpGet]
+            public IActionResult Create()
+            {
 
-        public IActionResult Update()
-        {
-            return View();
-        }
+                var restaurantlist = _context.Restaurants.Select(r => new
+                {
+                    Value = r.Id,
+                    Display = r.Name
+                }).ToArray();
+
+                var model = new Models.Sitting.Create
+
+                {
+                    Restaurants = new SelectList(restaurantlist.ToList(), "Value", "Display")
+                };
+
+                return View(model);
+            }
+            [HttpPost]
+            public async Task<IActionResult> Create(Models.Sitting.Create model)
+            {
+                if (ModelState.IsValid)
+                {
+                var sitting = new Sitting { Name = model.Name, StartTime = model.StartTime, EndTime = model.EndTime, Capacity = model.Capacity, RestaurantId = model.RestaurantId, IsClosed = model.IsClosed };
+                }
+                return View(model);
+
+            }
+            public IActionResult Delete()
+            {
+                return View();
+            }
+
+            public IActionResult Update()
+            {
+                return View();
+            }
     }
-}
+} 
+
--- ReservationProject/Areas/Admin/Models/Sitting/Create.cs
+++ ReservationProject/Areas/Admin/Models/Sitting/Create.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ReservationProject.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +11,27 @@ namespace ReservationProject.Areas.Admin.Models.Sitting
     public class Create
     {
         //Properties to create
+        [Required]
+        [Display(Name = "Name of Sitting")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Start Time")]
+        public DateTime StartTime { get; set; }
+        [Required]
+        [Display(Name = "End Time")]
+        public DateTime EndTime { get; set; }
+        [Required]
+        [Display(Name = "Capacity")]
+        public int Capacity { get; set; }
+        [Required]
+        [Display(Name = "Restaurant")]
+        public int RestaurantId { get; set; }
+
+        public SelectList Restaurants { get; set; }
+
+        public bool IsClosed { get; set; }
+
+
+
     }
 }
