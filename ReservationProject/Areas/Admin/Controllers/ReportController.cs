f1219f779cbbb41dc00e61144cd99c3e9fa7ab7c
DavidWHY312 <david.yau1@hotmail.com>
2021-10-24 18:39:46 +1100

    Update ReportController.cs
    ReportController inherits from admin base controller

--- ReservationProject/Areas/Admin/Controllers/ReportController.cs
+++ ReservationProject/Areas/Admin/Controllers/ReportController.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ReservationProject.Data;
 
 namespace ReservationProject.Areas.Admin.Controllers
 {
     [Area("Admin"), Authorize(Roles = "Admin")]
-    public class ReportController : Controller
+    public class ReportController : AdminAreaBaseController
     {
+        public ReportController(ApplicationDbContext context)
+         : base(context)
+        {
+
+        }
+
         public IActionResult Index()
         {
             return View();
