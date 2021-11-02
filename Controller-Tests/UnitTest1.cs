8c8d656ad93aba0e27a6b28974d31dd4399499df
LeeHikari <liam.isle@gmail.com>
2021-11-02 17:02:18 +1100

    Testing changes

--- Controller-Tests/UnitTest1.cs
+++ Controller-Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Controller_Tests
 {
-    [TestClass]
+    [TestClass()]
     public class UnitTest1
     {
         private ApplicationDbContext GetDatabaseContext()
@@ -29,13 +29,14 @@ namespace Controller_Tests
             var context = GetDatabaseContext();
             //var sitting = new Sitting();
             var model = new ReservationProject.Areas.Admin.Models.Sitting.Create
-            { Name = "Test",
+            {
+                Name = "Test",
                 SitStartTime = DateTime.Now,
                 SitEndTime = DateTime.Now.AddHours(3),
                 Capacity = 30,
                 RestaurantId = 1,
                 IsClosed = true,
-             };
+            };
 
             SittingController controller = new SittingController(context);
 
@@ -53,5 +54,32 @@ namespace Controller_Tests
 
 
         }
+        [TestMethod]
+        public void IndexTestNonNull()
+        {
+            //A - arrange (obj instance, setup test data)
+            SittingController controller = new SittingController(null);
+
+            //A - act (Call Method to Test)
+            var actualResult = controller.Index();
+
+            //A - assert (Check the return value)
+            Assert.IsNotNull(actualResult);
+        }
+
+        //[TestMethod]
+        //public void IndexTitleTest()
+        //{
+        //    //Arrange
+        //    HomeController controller = new HomeController(null);
+
+        //    //Act
+        //    ViewResult actualResult = (ViewResult)controller.Index();
+
+        //    //Assert
+        //    Assert.IsNotNull(actualResult.ViewData["Title"]);
+        //    Assert.IsInstanceOfType(actualResult.ViewData["Title"], typeof(string));
+        //    Assert.IsTrue(actualResult.ViewData["Title"].ToString().Contains("Index"));
+        //}
     }
 }
