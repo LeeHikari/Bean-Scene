7d86b221741206a9f4cf1cd862675cf3b25140d6
DavidWHY312 <david.yau1@hotmail.com>
2021-11-03 14:10:15 +1100

    Calendar JS moved to file

---
+++ ReservationProject/wwwroot/js/customCalender.js
@@ -0,0 +1,47 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+    function selectDate(date) {
+        $('.calendar-wrapper').updateCalendarOptions({
+            date: date,
+        });
+
+    document.getElementById("calendarDate").value = formatDate(date);
+            }
+
+    function formatDate(date) {
+                var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+    if (month.length < 2)
+    month = '0' + month;
+    if (day.length < 2)
+    day = '0' + day;
+
+    return [year, month, day].join('-');
+            }
+
+    var maxDate = new Date();
+    var todayDate = new Date();
+
+    todayDate.setHours(0, 0, 0, 0);
+    maxDate.setHours(0, 0, 0, 0);
+    maxDate.setMonth(todayDate.getMonth() + 6);
+
+    var defaultConfig = {
+        weekDayLength: 1,
+    date: new Date().toLocaleDateString(),
+    onClickDate: selectDate,
+    highlightSelectedWeek: false,
+    highlightSelectedWeekday: false,
+    todayButtonContent: "Reset",
+    min: todayDate,
+    max: maxDate,
+            };
+
+    $('.calendar-wrapper').calendar(defaultConfig);
+
