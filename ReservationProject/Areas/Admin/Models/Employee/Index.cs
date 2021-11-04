e17d8a31281b5971f8cdd34398ddd0a00e5945b4
DavidWHY312 <david.yau1@hotmail.com>
2021-11-04 18:17:58 +1100

    Employee Index Working

---
+++ ReservationProject/Areas/Admin/Models/Employee/Index.cs
@@ -0,0 +1,14 @@
+﻿using ReservationProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationProject.Areas.Admin.Models.Employee
+{
+    public class Index:Person
+    {
+        public List<Person> Staff { get; set; } = new List<Person>();
+        public List<Person> Admin { get; set; } = new List<Person>();
+    }
+}
