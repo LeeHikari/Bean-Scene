aba98ef9fabc12eeb1b1d3b4c479569466ec8d78
LeeHikari <liam.isle@gmail.com>
2021-11-07 12:50:41 +1100

    Sitting details

--- ReservationProject/Areas/Admin/Models/Sitting/Details.cs
+++ ReservationProject/Areas/Admin/Models/Sitting/Details.cs
@@ -1,7 +1,10 @@
-﻿namespace ReservationProject.Areas.Admin.Models.Sitting
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationProject.Areas.Admin.Models.Sitting
 {
     public class Details : Create
     {
+        [Required]
         public int Id { get; set; }
     }
 }
