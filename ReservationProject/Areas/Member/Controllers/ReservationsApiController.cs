e9867e28c506414f4563c802d9ba79d699d3d2e6
DavidWHY312 <david.yau1@hotmail.com>
2021-11-10 22:27:56 +1100

    API Authorization and package allows for bigger JSON
    Restricts and check for login details of app user

--- ReservationProject/Areas/Member/Controllers/ReservationsApiController.cs
+++ ReservationProject/Areas/Member/Controllers/ReservationsApiController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ReservationProject.Data;
@@ -11,38 +12,89 @@ using ReservationProject.Data;
 namespace ReservationProject.Areas.Member.Controllers
 {
 
-    [Authorize]
+    [Authorize(Roles ="Member,Admin,Staff")]
     [Route("api/Reservations")]
     
     [ApiController]
-    public class ReservationsControllerApi : ControllerBase
+    public class ReservationsApiController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ReservationsControllerApi(ApplicationDbContext context)
+        public ReservationsApiController(ApplicationDbContext context, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser>userManager)
         {
             _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager; 
         }
 
         // GET: api/ReservationsControllerApi
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Reservation>>> GetReservations()
         {
-            return await _context.Reservations.ToListAsync();
+            if (User.Identity.IsAuthenticated)
+            {
+                if (User.IsInRole("Member"))
+                {
+                    var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
+
+                    var reservations= await _context.Reservations
+                        .Include(rs => rs.ReservationSource)
+                        .Include(rst => rst.ReservationStatus)
+                        .Include(sr => sr.Sitting.Restaurant)
+                        .Include(s => s.Sitting)
+                        .Include(p => p.Person)
+                        .Where(p => p.Person.UserId == user.Id)
+                        .ToListAsync();
+
+                    if (reservations==null)
+                    {
+                        return NotFound();
+
+                    }
+                    return reservations;
+
+                }
+                return NotFound();
+
+            }
+            return NotFound();
+
         }
 
         // GET: api/ReservationsControllerApi/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Reservation>> GetReservation(int id)
         {
-            var reservation = await _context.Reservations.FindAsync(id);
-
-            if (reservation == null)
+            if (User.Identity.IsAuthenticated)
             {
-                return NotFound();
-            }
+                if (User.IsInRole("Member"))
+                {
+                    var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name);
 
-            return reservation;
+                    var reservation = await _context.Reservations
+                        .Include(rs => rs.ReservationSource)
+                        .Include(rst => rst.ReservationStatus)
+                        .Include(sr => sr.Sitting.Restaurant)
+                        .Include(s => s.Sitting)
+                        .Include(p => p.Person)
+                        .Where(p => p.Person.UserId == user.Id)
+                        .FirstAsync(r => r.Id == id);
+
+                    if (reservation == null)
+                    {
+                        return NotFound();
+                    }
+                    return reservation;
+
+                }
+
+                return NotFound();
+
+            }
+            return NotFound();
+
         }
 
         // PUT: api/ReservationsControllerApi/5
