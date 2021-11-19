79fed7f42d73232a0a188c849493ea9af8c153d6
Steven Weston <stevengweston@hotmail.com>
2021-11-19 14:46:05 +1100

    azureIntegrated

--- ReservationProject/Areas/Admin/Controllers/ReservationController.cs
+++ ReservationProject/Areas/Admin/Controllers/ReservationController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using ReservationProject.Data;
 using ReservationProject.Service;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -260,6 +261,31 @@ namespace ReservationProject.Areas.Admin.Controllers
             {
                 return StatusCode(500);
             }
+        }
+
+        [HttpPost]
+        public JsonResult GetTimes(int id)
+        {
+            Task<Sitting> SittingTimes = _context.Sittings.FirstOrDefaultAsync(s => s.Id == id);
+            SittingTimes.Wait();
+            var sitting = SittingTimes.Result;
+
+            List<DateTime> dates = new List<DateTime>();
+
+            if (sitting != null)
+            {
+                dates.Add(sitting.StartTime);
+                dates.Add(sitting.EndTime);
+            }
+            return Json(dates);
+        }
+
+        [HttpPost]
+        public JsonResult GetSittings(string date)
+        {
+            var sittingsList = _context.Sittings.Where(s => s.StartTime.Date == DateTime.Parse(date).Date);
+
+            return Json(sittingsList);
         }
 
 
